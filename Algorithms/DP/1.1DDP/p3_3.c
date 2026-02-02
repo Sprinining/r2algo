a4b91dd73dd734faf1075ca58089a921282c04f6
Sylvan <sprinining@gmail.com>
2026-02-02 13:05:04 +0800

    code: 优化版2

--- Algorithms/DP/1.1DDP/p3_3.c
+++ Algorithms/DP/1.1DDP/p3_3.c
@@ -3,28 +3,22 @@
 #include <string.h>
 
 // 自底向上严格位置依赖
+// dp[n] 含义由 s[0...n] 的解码方法改为长度为 n 的字符串的解码方法
 int numDecodings(char* s) {
-    int len = strlen(s);
-    // 只有一个字符
-    if (len == 1) return s[0] == '0' ? 0 : 1;
-
-    // 两个及以上
     // 首先排除前导零
     if (s[0] == '0') return 0;
+    int len = strlen(s);
+    // 只有一个字符
+    if (len == 1) return 1;
 
-    int* dp = (int*)malloc(sizeof(int) * len);
+    // 两个及以上
+    int* dp = (int*)malloc(sizeof(int) * (len + 1));
+    // 避免讨论长度为 2 的字符串的情况
     dp[0] = 1;
-    int val = (s[0] - '0') * 10 + (s[1] - '0');
-    if (val == 10 || val == 20 || (val > 26 && (val % 10) != 0)) {
-        dp[1] = 1;
-    } else if ((val >= 11 && val <= 19) || (val >= 21 && val <= 26)) {
-        dp[1] = 2;
-    } else {
-        dp[1] = 0;
-    }
+    dp[1] = 1;
 
-    for (int i = 2; i < len; i++) {
-        int val = (s[i - 1] - '0') * 10 + (s[i] - '0');
+    for (int i = 2; i <= len; i++) {
+        int val = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
         if (val == 0 || (val > 26 && (val % 10 == 0))) {
             // 非法编码：00,30,40,50...
             dp[i] = 0;
@@ -40,5 +34,5 @@ int numDecodings(char* s) {
         }
     }
 
-    return dp[len - 1];
+    return dp[len];
 }
