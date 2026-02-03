d81b6ea249b0c3821d506a01c5d90e35b69460fd
Sylvan <sprinining@gmail.com>
2026-02-04 00:16:54 +0800

    code: 最长有效括号

---
+++ Algorithms/DP/1.1DDP/p7_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int longestValidParentheses(char* s) {
+    int len = strlen(s);
+    if (len < 2) return 0;
+
+    int max = 0;
+    // dp[i] 返回字符串长度为 i 且必须以末尾元素结尾的最长有效括号子串的长度
+    int* dp = (int*)calloc(len + 1, sizeof(int));
+    for (int i = 2; i <= len; i++) {
+        if (s[i - 1] == ')') {
+            // 上一个有效串的前一个位置
+            int index = i - 2 - dp[i - 1];
+            if (index >= 0 && s[index] == '(')
+                dp[i] = dp[i - 1] + 2 + ((index >= 1) ? dp[index] : 0);
+        }
+        if (dp[i] > max) max = dp[i];
+    }
+    // 最终返回 dp 数组中最大的
+    return max;
+}
