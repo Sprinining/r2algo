5c4b2faa0bf74ba357d6e6e929875bda62ae2bf1
Sylvan <sprinining@gmail.com>
2026-02-10 11:47:41 +0800

    code: 矩阵中的最长递增路径

--- Algorithms/DP/2.2DDP/p5_2.c
+++ Algorithms/DP/2.2DDP/p5_2.c
@@ -1,13 +1,17 @@
+#include <stdlib.h>
+
 int rows, columns;
 int directions[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 int mmax(int a, int b) { return a > b ? a : b; }
+int** dp;
 
 // 从 matrix[i][j] 出发的最长递增路径
 int func(int** matrix, int i, int j, int before) {
     if (i < 0 || j < 0 || i >= rows || j >= columns) return 0;
     // 严格递增
     if (matrix[i][j] <= before) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
     // 遍历四个方向
     int res = 0;
     for (int k = 0; k < 4; k++) {
@@ -16,12 +20,18 @@ int func(int** matrix, int i, int j, int before) {
         res = mmax(res, func(matrix, new_i, new_j, matrix[i][j]));
     }
     // 包含自身
-    return res + 1;
+    dp[i][j] = res + 1;
+    return dp[i][j];
 }
 
 int longestIncreasingPath(int** matrix, int matrixSize, int* matrixColSize) {
     rows = matrixSize;
     columns = matrixColSize[0];
+    dp = (int**)malloc(sizeof(int*) * rows);
+    for (int i = 0; i < rows; i++) {
+        dp[i] = (int*)malloc(sizeof(int) * columns);
+        memset(dp[i], -1, sizeof(int) * columns);
+    }
 
     int res = 0;
     for (int i = 0; i < rows; i++) {
