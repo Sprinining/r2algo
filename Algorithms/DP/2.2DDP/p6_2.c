4217ba4d45b709e0114c74589eef1a25b7935df2
Sylvan <sprinining@gmail.com>
2026-02-28 16:19:27 +0800

    code: 不同的子序列

--- Algorithms/DP/2.2DDP/p6_2.c
+++ Algorithms/DP/2.2DDP/p6_2.c
@@ -1,25 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+
+int MOD = 1000000007;
+
 // 空间优化
 int numDistinct(char* s, char* t) {
-    const int MOD = 1e9 + 7;
-    int lenS = strlen(s);
-    int lenT = strlen(t);
-    // dp[i][j]表示在s中前缀长度为i的字符串所包含的所有子序列中，有多少个子序列等于t中前缀长度为j的字符串
-    // i、j表示前缀长度，而不是字符串中的下标
-    int dp[lenT + 1];
-    // 第一列全1，表示空串的时候匹配
-    // 第一行除了第一个元素，全0，不可能匹配
-    memset(dp, 0, sizeof(int) * (lenT + 1));
+    int len_s = strlen(s);
+    int len_t = strlen(t);
+    int rows = len_s + 1;
+    int columns = len_t + 1;
+
+    // dp[i][j] 表示在 s 的前 i 个字符里，能组成 t 的前 j 个字符的子序列个数
+    // 问题关键是 s[i] 到底用不用
+    int* dp = (int*)malloc(sizeof(int) * columns);
+    // 第一列全 1，表示 t 为空串时会出现在 s 的子序列中一次
+    // 第一行除了第一个，其余为 0，表示空串 t 的子序列中不可能出现非空串 t
+    memset(dp, 0, sizeof(int) * columns);
     dp[0] = 1;
 
-    // 当前位置依赖于左上角和上方的格子，所以从上往下，从右往左填
-    for (int i = 1; i <= lenS; ++i) {
-        for (int j = lenT; j >= 1; j--) {
-            // 情况1：s[i]不选，则问题转化为dp[i -
-            // 1][j]，即上方格子，此时dp[j]不用变动，直接就表示上方格子
-            // 情况2：s[i]选，但是前提条件是末尾字符相同，问题转化为dp[i - 1][j
-            // - 1]，dp[j-1]尚未更新，表示左上方格子
-            if (s[i - 1] == t[j - 1]) dp[j] = (dp[j] + dp[j - 1]) % MOD;
+    int before;
+    int top_left;
+    for (int i = 1; i < rows; i++) {
+        top_left = 1;
+        for (int j = 1; j < columns; j++) {
+            before = dp[j];
+            // 情况 1：根本不能用 s[i]，问题等价于在 s 的前 i-1 个字符里，拼 t 的前 j 个字符
+
+            // 情况 2：可以用 s[i]，但前提是 s[i-1] == t[j-1]，前面必须已经匹配好 t 的前 j-1 个字符
+            if (s[i - 1] == t[j - 1]) {
+                // 结果要把选和不选的可能累计起来
+                dp[j] = ((long long)dp[j] + top_left) % MOD;
+            }
+
+            top_left = before;
         }
     }
-    return dp[lenT];
+
+    return dp[len_t];
 }
+/*
+    为什么不是 dp[i][j-1]？
+        dp[i][j-1] 是用 s 前 i 个字符拼 t 前 j-1 个字符，现在目标是拼 t 的前 j 个字符，少一个字符不等价
+        而我们只是在决定：当前 s[i-1] 用还是不用，所以只会从：s 少一个字符（i-1）转移过来
+*/
