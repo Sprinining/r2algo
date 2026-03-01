395bb78355bdc955b6e12f8653764df1c078b7cf
Sylvan <sprinining@gmail.com>
2026-03-01 21:55:22 +0800

    refactor: m1n

--- Algorithms/DP/2.2DDP/p1_3.c
+++ Algorithms/DP/2.2DDP/p1_3.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 
+int m1n(int a, int b) { return a > b ? b : a; }
+
 // 严格位置依赖
 int minPathSum(int** grid, int gridSize, int* gridColSize) {
     int row = gridSize;
     int column = gridColSize[0];
+    // dp[i][j] 表示到达 grid[i][j] 所需要的最小路径和
     int** dp = malloc(sizeof(*dp) * row);
     int* data = malloc(sizeof(*data) * row * column);
     for (int i = 0; i < row; i++) dp[i] = data + i * column;
@@ -16,15 +19,9 @@ int minPathSum(int** grid, int gridSize, int* gridColSize) {
         dp[0][j] = temp;
     }
 
-    for (int i = 1; i < row; i++) {
-        for (int j = 1; j < column; j++) {
-            if (dp[i - 1][j] < dp[i][j - 1]) {
-                dp[i][j] = dp[i - 1][j] + grid[i][j];
-            } else {
-                dp[i][j] = dp[i][j - 1] + grid[i][j];
-            }
-        }
-    }
+    for (int i = 1; i < row; i++)
+        for (int j = 1; j < column; j++) 
+            dp[i][j] = m1n(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
 
     return dp[row - 1][column - 1];
 }
