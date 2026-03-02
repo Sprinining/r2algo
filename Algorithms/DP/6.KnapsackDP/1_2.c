e5a091b8778cedb5082851a8257110422f21b563
Sylvan <sprinining@gmail.com>
2026-03-02 15:33:19 +0800

    refactor: 带返回值

--- Algorithms/DP/6.KnapsackDP/1_2.c
+++ Algorithms/DP/6.KnapsackDP/1_2.c
@@ -4,34 +4,30 @@
 int t, m;
 int** choice;
 int* data;
-int res;
 
 int mmax(int a, int b) { return a > b ? a : b; }
 
+// 带返回值
 // 返回最大的总价值
-void func(int time_left, int total_val, int cur_idx) {
+int func(int time_left, int cur_idx) {
     // 没得选或者背包满了
-    if (cur_idx == m || time_left == 0) {
-        res = mmax(res, total_val);
-        return;
-    }
+    if (cur_idx == m || time_left == 0) return 0;
 
     // 不选 choice[cur_idx]
-    func(time_left, total_val, cur_idx + 1);
+    int p1 = func(time_left, cur_idx + 1);
     // 选 choice[cur_idx]，前提不超过背包容量
+    int p2 = 0;
     if (time_left >= choice[cur_idx][0])
-        func(time_left - choice[cur_idx][0], total_val + choice[cur_idx][1], cur_idx + 1);
+        p2 = func(time_left - choice[cur_idx][0], cur_idx + 1) + choice[cur_idx][1];
+    return mmax(p1, p2);
 }
 
 int main() {
-    res = 0;
     scanf("%d %d", &t, &m);
     choice = malloc(sizeof(*choice) * m);
     data = malloc(sizeof(*data) * m * 2);
     for (int i = 0; i < m; i++) choice[i] = data + i * 2;
     for (int i = 0; i < m; i++) scanf("%d %d", &choice[i][0], &choice[i][1]);
 
-    func(t, 0, 0);
-
-    printf("%d", res);
+    printf("%d", func(t, 0));
 }
