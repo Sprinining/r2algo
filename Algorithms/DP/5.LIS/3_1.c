cc275965cdf7ad6c7e56c94c749782c06812f701
Sylvan <sprinining@gmail.com>
2026-03-11 21:27:10 +0800

    code: 使数组 K 递增的最少操作次数

---
+++ Algorithms/DP/5.LIS/3_1.c
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{public :
+                   // 大于 target 的左边界
+                   int binarySearch(vector<int> & ends, int len, int target){int left = 0;
+int right = len - 1;
+int mid;
+while (left <= right) {
+    mid = left + ((right - left) >> 1);
+    if (ends[mid] > target) {
+        right = mid - 1;
+    } else {
+        left = mid + 1;
+    }
+}
+return left;
+}
+
+// 时间复杂度 O(n * logn)
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    // ends[i] 表示所有长度为 i + 1 的不下降子序列的最小结尾
+    // [0, len-1] 是有效区，有效区内的数字非递减
+    vector<int> ends(n);
+    // len 表示 ends 数组目前的有效区长度
+    int len = 0;
+    for (int i = 0, pos; i < n; ++i) {
+        pos = binarySearch(ends, len, nums[i]);
+        if (pos == len) {
+            // 找不到就扩充 ends
+            ends[len++] = nums[i];
+        } else {
+            // 找到了就更新成更小的 nums[i]
+            ends[pos] = nums[i];
+        }
+    }
+    return len;
+}
+
+int kIncreasing(vector<int>& arr, int k) {
+    int n = arr.size();
+    int res = 0;
+    // 分为 k 组
+    for (int i = 0; i < k; ++i) {
+        vector<int> temp;
+        for (int j = i; j < n; j += k) temp.emplace_back(arr[j]);
+        // 累加这一组需要修改的数字
+        res += temp.size() - lengthOfLIS(temp);
+    }
+    return res;
+}
+}
+;
