906dcd5ff1bfe03fa6252b1a7841580c9471a0c1
Sylvan <sprinining@gmail.com>
2026-04-11 16:13:57 +0800

    code: 最长数对链

--- Algorithms/DP/5.LIS/4_1.c
+++ Algorithms/DP/5.LIS/4_1.c
@@ -1,45 +1,46 @@
-#include <vector>
-#include <algorithm>
+#include <stdlib.h>
 
-using namespace std;
+int m1n(int a, int b) { return a > b ? b : a; }
 
-class Solution {
-public:
-    int binarySearch(vector<int> &ends, int len, int target) {
-        int left = 0;
-        int right = len - 1;
-        int mid;
-        while (left <= right) {
-            mid = left + ((right - left) >> 1);
-            if (ends[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+int cmp(const void* a, const void* b) {
+    int* ea = *(int**)a;
+    int* eb = *(int**)b;
+    // 根据第一个元素升序排序
+    return ea[0] - eb[0];
+}
+
+int binarySearch(int* ary, int len, int target) {
+    int left = 0;
+    int right = len - 1;
+    int mid;
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+        if (ary[mid] >= target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
-        return left;
     }
+    return left;
+}
 
-    int findLongestChain(vector<vector<int>> &pairs) {
-        // 按照数对中第一个数增序
-        sort(begin(pairs), end(pairs),
-             [](vector<int> &v1, vector<int> &v2) {
-                 return v1[0] < v2[0];
-             });
-        int n = pairs.size();
-        vector<int> ends(n);
-        int len = 0;
-        for (int i = 0, pos; i < n; ++i) {
-            // 根据数对中第一个数字查
-            pos = binarySearch(ends, len, pairs[i][0]);
-            if (pos == len) {
-                // 插入的是数对中的第二个数字
-                ends[len++] = pairs[i][1];
-            } else {
-                // 改成较小的
-                ends[pos] = min(ends[pos], pairs[i][1]);
-            }
+int findLongestChain(int** pairs, int pairsSize, int* pairsColSize) {
+    qsort(pairs, pairsSize, sizeof(*pairs), cmp);
+
+    // ends[i] 表示所有长度为 i+1 的递增子序列的最小结尾
+    int* ends = malloc(sizeof(*ends) * pairsSize);
+    int len = 0;
+
+    for (int i = 0; i < pairsSize; ++i) {
+        // 根据第一个数查
+        int pos = binarySearch(ends, len, pairs[i][0]);
+        // 插入的是第二个数
+        if (pos == len) {
+            ends[len++] = pairs[i][1];
+        } else {
+            // 最小结尾
+            ends[pos] = m1n(ends[pos], pairs[i][1]);
         }
-        return len;
     }
-};
+    return len;
+}
