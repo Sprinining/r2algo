a0f8821a80a966dff0f3be4fe2366bae5aa9c442
Sylvan <sprinining@gmail.com>
2026-04-12 22:19:21 +0800

    code: 简化

--- Algorithms/DP/4.MaximumSubarraySum/2_1.c
+++ Algorithms/DP/4.MaximumSubarraySum/2_1.c
@@ -5,7 +5,6 @@
 // 不能选相邻元素的最大累加和问题
 int rob(int* nums, int numsSize) {
     if (numsSize == 1) return nums[0];
-    if (numsSize == 2) return MMAX(nums[0], nums[1]);
     int* dp = malloc(sizeof(*dp) * numsSize);
     dp[0] = nums[0];
     dp[1] = MMAX(nums[0], nums[1]);
--- Algorithms/DP/4.MaximumSubarraySum/2_2.c
+++ Algorithms/DP/4.MaximumSubarraySum/2_2.c
@@ -5,10 +5,9 @@
 // 空间压缩
 int rob(int* nums, int numsSize) {
     if (numsSize == 1) return nums[0];
-    if (numsSize == 2) return MMAX(nums[0], nums[1]);
     int left = nums[0];
     int mid = MMAX(nums[0], nums[1]);
-    int right;
+    int right = mid;
     for (int i = 2; i < numsSize; ++i) {
         // 当前位置偷不偷两种情况
         right = MMAX(left + nums[i], mid);
