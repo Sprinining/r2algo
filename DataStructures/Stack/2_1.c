8b10d45d038c05107fe768fb0080e75f1743f2f1
Sylvan <sprinining@gmail.com>
2026-05-31 18:02:36 +0800

    code: 每日温度

---
+++ DataStructures/Stack/2_1.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#define MAXN 100005
+
+// 递减栈
+int stk[MAXN];
+int sz;
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize,
+                       int* returnSize) {
+    int n = temperaturesSize;
+    *returnSize = n;
+    int* res = calloc(n, sizeof(int));
+    sz = 0;
+
+    for (int i = 0; i < n; ++i) {
+        while (sz > 0 && temperatures[stk[sz - 1]] < temperatures[i]) {
+            int top = stk[sz - 1];
+            res[top] = i - top;
+            --sz;
+        }
+        stk[sz++] = i;
+    }
+    return res;
+}
