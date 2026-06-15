2ed0944fda8f87cafb02b49299bcaf0dd241f388
Sylvan <sprinining@gmail.com>
2026-06-15 08:50:38 +0800

    code: 优化

--- Techniques/SlidingWindow/2_1.c
+++ Techniques/SlidingWindow/2_1.c
@@ -14,19 +14,15 @@ int lengthOfLongestSubstring(char* s) {
     // 只在循环结束自增 r
     // 循环内部自增 l 去重
     for (int l = 0, r = 0; r < n; ++r) {
-        if (++cnt[s[r]] == 1) {
-            // 字符种数加一
-            ++kind;
-            res = MMAX(res, kind);
-            continue;
-        }
+        ++cnt[s[r]];
+        if (cnt[s[r]] == 1) ++kind;
 
-        // 一个字符在窗口中出现多次
-        // 去除重复的
         while (kind != r - l + 1) {
-            if (--cnt[s[l]] == 0) --kind;
+            --cnt[s[l]];
+            if (cnt[s[l]] == 0) --kind;
             ++l;
         }
+        res = MMAX(res, kind);
     }
 
     return res;
