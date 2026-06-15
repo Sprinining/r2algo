e06e90a162e134351a6602021f7adfeaf88b99ff
Sylvan <sprinining@gmail.com>
2026-06-15 09:14:15 +0800

    code: 优化

--- Techniques/SlidingWindow/4_5.c
+++ Techniques/SlidingWindow/4_5.c
@@ -1,9 +1,10 @@
 int canCompleteCircuit(int* gas, int gasSize, int* cost, int costSize) {
     int n = gasSize;
     int oil = 0;
+    int len = n << 1;
 
     // 外循环为 r
-    for (int l = 0, r = 0; r < 2 * n; ++r) {
+    for (int l = 0, r = 0; r < len; ++r) {
         oil += gas[r % n] - cost[r % n];
 
         while (oil < 0) {
