a4d0aea6494c573a18112fdba01fd85a56db9953
Sylvan <sprinining@gmail.com>
2026-06-17 21:02:43 +0800

    code: 排序数组-归并

---
+++ Algorithms/DivideAndConquer/1_1.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+
+void merge(int* nums, int left, int mid, int right, int* temp) {
+    int p = left, q = mid + 1;
+    int idx = 0;
+    while (p <= mid && q <= right) {
+        if (nums[p] < nums[q]) {
+            temp[idx++] = nums[p++];
+        } else {
+            temp[idx++] = nums[q++];
+        }
+    }
+
+    while (p <= mid) temp[idx++] = nums[p++];
+    while (q <= right) temp[idx++] = nums[q++];
+    memcpy(&nums[left], temp, sizeof(int) * idx);
+}
+
+void divide(int* nums, int left, int right, int* temp) {
+    if (left >= right) return;
+    int mid = left + ((right - left) >> 1);
+    // 递归拆分
+    divide(nums, left, mid, temp);
+    divide(nums, mid + 1, right, temp);
+    // 合并
+    merge(nums, left, mid, right, temp);
+}
+
+int* sortArray(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+    int* temp = malloc(sizeof(int) * numsSize);
+    divide(nums, 0, numsSize - 1, temp);
+    return nums;
+}
